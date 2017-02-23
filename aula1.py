bfa8dce5a4f72b2563e5ff5adbf4df1801888142
Leonardo Barros <nickolback@gmail.com>
2017-02-23 09:51:42 -0300

    Aula 1

---
+++ aula1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+nome = 'Leonardo Barros'
+profissao = 'Sistemas de informação'
+
+print('Nome: %s, Profissão: %s' % (nome, profissao))
+
+
+#numeros
+salario = 7500.00
+desconto = 500.00
+total = salario - desconto
+
+print('Salário: %s, Desconto: %s' % (salario, desconto))
+print('Total: %s' % (total))
+
