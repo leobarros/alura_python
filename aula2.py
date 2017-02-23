74d22341f239c217f87fa1d6a2a0784e57f8ae8e
Leonardo Barros <nickolback@gmail.com>
2017-02-23 10:43:32 -0300

    Aula2 - Listas

---
+++ aula2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# aula 2 do curso
+# usando listas
+
+frutas = ['banana', 'goiaba', 'melancia', 'carambola']
+print(frutas[0:2])
+
+frutas.append('abacaxi')
+print(frutas)
+
+frutas.append('ameixa')
+print(frutas)
+
+frutas.remove('banana')
+print(frutas)
