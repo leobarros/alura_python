bf7501451a26b39c3172d852219a5130d74d75da
Leonardo Sales <lsales@globosat.com.br>
2017-03-12 17:17:00 -0300

    Aula 6 - Organizando o codigo

---
+++ python/app.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+def cadastrar(nomes):
+    nome = raw_input('Digite o nome: ')
+    nomes.append(nome)
+
+def listar(nomes):
+    print nomes
+
+def menu():
+    nomes = []
+    escolha = ''
+
+    while (escolha != '0'):
+        print '\n1 - Cadastrar nome\n2 - Listar\n0 - Sair\n'
+        escolha = raw_input('Escolha uma opção: ')
+
+        if (escolha == '1'):
+            cadastrar(nomes)
+        if (escolha == '2'):
+            print('\nListando nomes')
+            listar(nomes)
+
+menu()
