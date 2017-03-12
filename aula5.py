655ddf6e87d87aeab440901993ad3f3b966a1356
Leonardo Sales <lsales@globosat.com.br>
2017-03-12 16:28:34 -0300

    Cadastrando perfis

---
+++ aula5.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# criando uma funcao para cadastrar nomes
+# É necessario ter uma lista vazia chamada nome = []
+
+nomes = []
+
+def cadastrar(nomes):
+    nome = raw_input('Digite o nome: ')
+    nomes.append(nome)
