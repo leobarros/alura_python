e9004c3892d03fcc0ce3d9fade6b76aa016fe1ef
Leonardo Sales <lsales@globosat.com.br>
2017-04-08 15:37:52 -0300

    Exemplo de classe alunos

---
+++ alunos.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+class Adicionar(object):
+    'Classe para adicionar dados dos alunos'
+
+    def __init__(self, nome, sala, turma, turno):
+        self.nome = nome
+        self.sala = sala
+        self.turma = turma
+        self.turno = turno
